cb47073e8d7a48ef266d319b9d6867ab493deec1
PatricioPallua <patricio.pallua@mercadolibre.com>
2022-01-10 15:33:41 -0300

    Test Update

--- internal/sales/service_test.go
+++ internal/sales/service_test.go
@@ -3,6 +3,7 @@ package sales
 import (
 	"testing"
 
+	"github.com/MatiasDBonis/hackaton-bootcamp-go.git/internal/domain"
 	"github.com/MatiasDBonis/hackaton-bootcamp-go.git/pkg/db"
 	"github.com/MatiasDBonis/hackaton-bootcamp-go.git/pkg/parser"
 	"github.com/stretchr/testify/assert"
@@ -43,4 +44,59 @@ func TestInsertCustomersExistents(t *testing.T) {
 	cant, err = service.InsertAll(parsedSales)
 	assert.Equal(t, 0, cant)
 	assert.NotNil(t, err)
+}
+
+func TestUpdateOK(t *testing.T) {
+	db, err := db.InitTxdb()
+	assert.Nil(t, err)
+
+	newSales := []domain.Sales{
+		{
+			Id:        1,
+			IdInvoice: 1,
+			IdProduct: 1,
+			Quantity:  10,
+		},
+	}
+
+	salesEdited := domain.Sales{
+		Id:        1,
+		IdInvoice: 2,
+		IdProduct: 2,
+		Quantity:  20,
+	}
+
+	repo := NewRepository(db)
+	defer db.Close()
+
+	service := NewService(repo)
+
+	service.InsertAll(newSales)
+
+	salesResult, err := service.Update(salesEdited)
+
+	assert.Nil(t, err)
+	assert.Equal(t, salesEdited, salesResult)
+}
+
+func TestUpdateIdInexistent(t *testing.T) {
+	db, err := db.InitTxdb()
+	assert.Nil(t, err)
+
+	salesEdited := domain.Sales{
+		Id:        2,
+		IdInvoice: 1,
+		IdProduct: 1,
+		Quantity:  10,
+	}
+
+	repo := NewRepository(db)
+	defer db.Close()
+
+	service := NewService(repo)
+
+	salesResult, err := service.Update(salesEdited)
+
+	assert.Error(t, err)
+	assert.NotEqual(t, salesEdited, salesResult)
 }
